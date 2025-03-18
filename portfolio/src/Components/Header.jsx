d3d0b471946b553ee3090c46ad3d7e308e7e25d2
Santiago <santiagox46@hotmail.com>
2025-03-18 09:04:09 -0400

    Changement du nom pour le mien

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
             <div className="profile">
                 <ProfilePicture pictureURL={"https://media.gettyimages.com/id/1407293956/photo/silhouette-of-a-man-interacting-with-virtual-computer-graphics.jpg?s=2048x2048&w=gi&k=20&c=m21VDHEY5cMLtc9XaGq-ITuLyxAnY5gtfU2l5A3gi6M="}  />
                 <div>
-                    <h1>Jean Dupont</h1>
+                    <h1>Santiago C</h1>
                     <p>Développeur mobile spécialisé en applications multiplateformes et UX/UI design</p>
                 </div>
             </div>
