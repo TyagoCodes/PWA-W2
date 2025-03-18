e4913af63f80b2573dd5b3a338793a34035b7174
Santiago <santiagox46@hotmail.com>
2025-03-18 09:44:47 -0400

    nopicture or picture for Header.jsx

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,13 +1,32 @@
 import ProfilePicture from "./ProfilePicture.jsx";
 
+const name = "Santiago C";
+const description = "Développeur mobile spécialisé en applications multiplateformes et UX/UI design";
+const noPicture = false;
+
 export function Header() {
+    if(noPicture){
+        return (
+            <header>
+                <div className="profile">
+                    <div>
+                        <h1> {name} </h1>
+                        <p> {description} </p>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+
+
+
     return (
         <header>
             <div className="profile">
-                <ProfilePicture pictureURL={"https://media.gettyimages.com/id/1407293956/photo/silhouette-of-a-man-interacting-with-virtual-computer-graphics.jpg?s=2048x2048&w=gi&k=20&c=m21VDHEY5cMLtc9XaGq-ITuLyxAnY5gtfU2l5A3gi6M="}  />
+                <ProfilePicture pictureURL={"https://media.gettyimages.com/id/1407293956/photo/silhouette-of-a-man-interacting-with-virtual-computer-graphics.jpg?s=2048x2048&w=gi&k=20&c=m21VDHEY5cMLtc9XaGq-ITuLyxAnY5gtfU2l5A3gi6M="} fallbackText={'Ma photo'}/>
                 <div>
-                    <h1>Santiago C</h1>
-                    <p>Développeur mobile spécialisé en applications multiplateformes et UX/UI design</p>
+                    <h1> {name} </h1>
+                    <p> {description} </p>
                 </div>
             </div>
         </header>
